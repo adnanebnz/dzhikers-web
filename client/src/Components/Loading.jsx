9a9b00d067595765fd3ca20fc38235cd87e57646
skillz <skillzdev@hotmail.com>
2023-04-09 16:43:01 +0100

    test

--- client/src/Components/Loading.jsx
+++ client/src/Components/Loading.jsx
@@ -1,7 +1,7 @@
 import { CircularProgress } from "@mui/material";
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center max-h-screen max-w-5xl">
+    <div className="flex items-center justify-center h-screen max-w-5xl">
       <CircularProgress />
     </div>
   );
